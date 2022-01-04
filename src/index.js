3145658299936ae27c087a01e43ac42a020a2853
sla6ak <slabakxaker@gmail.com>
2022-01-04 11:03:50 +0200

    add inputs

--- src/index.js
+++ src/index.js
@@ -7,35 +7,40 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}50`;
 }
 const DEBOUNCE_DELAY = 300;
-const MAX_COUNTRY = 10;
 const inputCounrtry = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 
 inputCounrtry.addEventListener('input', debounce(onCountryChenge, DEBOUNCE_DELAY));
 
 function onCountryChenge() {
+  let MAX_COUNTRY = document.querySelector('#max-country').value;
+  let DETALY = document.querySelector('#detaly').checked;
   const nameCountry = inputCounrtry.value.trim();
-  console.log(nameCountry);
   if (nameCountry) {
     fetchCountries(nameCountry)
       .then(arrCountrys => {
-        console.log(arrCountrys);
         if (arrCountrys.length > MAX_COUNTRY) {
           countryList.innerHTML = '';
           Notiflix.Notify.info(`Too many matches found. Please enter a more specific name.`);
         } else {
           const htmlEl = arrCountrys => {
-            return arrCountrys.map(country => {
-              console.log('флаг в консоль вам: ', country.flags.svg);
-              return `<li style = 'background-color:${getRandomHexColor()}'>
+            return arrCountrys.map((country, index, arrey) => {
+              if (DETALY == true || arrey.length === 1) {
+                return `<li style = 'background-color:${getRandomHexColor()}'>
               <p>Страна: <span class="name-official">${country.name.official}</span></p>
-              <p>столица: <span class="capital">${country.capital.join('')}</span></p>
-              <div>флаг: <img class='flagImg' src="${country.flags.svg}" alt="флаг страны" /></div>
-              <p>языки: <span class="languages">${Object.values(country.languages).join(
+              <p>Столица: <span class="capital">${country.capital.join('')}</span></p>
+              <div>Флаг: <img class='flagImg' src="${country.flags.svg}" alt="флаг страны" /></div>
+              <p>Языки: <span class="languages">${Object.values(country.languages).join(
                 '',
               )}</span></p>
-              <p>население: <span class="population">${country.population}</span></p>
+              <p>Население: <span class="population">${country.population}</span></p>
               </li>`;
+              } else {
+                return `<li style = 'background-color:${getRandomHexColor()}'>
+                <div><img class='flagImg' src="${country.flags.svg}" alt="флаг страны" /></div>
+                <p><span class="name-official">${country.name.official}</span></p>
+                </li>`;
+              }
             });
           };
           countryList.innerHTML = htmlEl(arrCountrys).join('');
@@ -45,11 +50,7 @@ function onCountryChenge() {
         countryList.innerHTML = '';
         Notiflix.Notify.failure(`❌ Oops, there is no country with that name`);
       });
+  } else {
+    countryList.innerHTML = '';
   }
 }
-
-//<p>Страна: <span class="name-official">${name.official}</span></p>
-// <p>флаг: <span class="flags">${flags}</span></p>
-//<p>столица: <span class="capital">${capital}</span></p>
-//<p>население: <span class="population">${population}</span></p>
-//<p>языки: <span class="languages">${languages}</span></p>
